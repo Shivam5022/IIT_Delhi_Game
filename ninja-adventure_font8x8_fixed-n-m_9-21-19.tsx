44ba6e4e1da2bb53a4bda9d95af26463fcc90575
Shivam Verma <83013259+Shivam5022@users.noreply.github.com>
2022-05-03 17:14:10 +0530

    Delete ninja-adventure_font8x8_fixed-n-m_9-21-19.tsx

--- ninja-adventure_font8x8_fixed-n-m_9-21-19.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="ninja-adventure_font8x8_fixed-n-m_9-21-19" tilewidth="32" tileheight="32" tilecount="6" columns="3">
- <image source="../../../Downloads/ninja-adventure_font8x8_fixed-n-m_9-21-19.png" width="120" height="64"/>
-</tileset>
