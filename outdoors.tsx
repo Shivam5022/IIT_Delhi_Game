bf8b29abfc0a80a7e7902eb7e88a398488e1beaa
Shivam Verma <83013259+Shivam5022@users.noreply.github.com>
2022-05-03 17:14:21 +0530

    Delete outdoors.tsx

--- outdoors.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="outdoors" tilewidth="32" tileheight="32" tilecount="196" columns="14">
- <image source="../../../Downloads/modern-city-assets/outdoors.png" width="464" height="464"/>
-</tileset>
